34f5322a3a76979af0d8ed78ab1bed22ab5e70ab
Patryk <patro272@wp.pl>
2022-05-07 18:18:46 +0200

    Code optimalization for third.py file

--- third.py
+++ third.py
@@ -13,20 +13,22 @@ percent = 8
 file = open("Runs.txt", 'r')
 for i in file:
     arr = i.split("; ")
-    S_arr_len = arr[2].split()
-    percent_arr = arr[3].split()
     bef_val = arr[0].split()
     aft_val = arr[1].split()
+    S_arr_len = arr[2].split()
+    percent_arr = arr[3].split()
+    val_of_S_arr_len = int(S_arr_len[len(S_arr_len)-1])
+    val_to_add = float(aft_val[len(aft_val)-1])*100
     if int(percent_arr[len(percent_arr)-1]) == percent:
-        if int(S_arr_len[len(S_arr_len)-1]) == S_length and len(after_ga_values_15) < 10:
+        if val_of_S_arr_len == S_length and len(after_ga_values_15) < 10:
             bef_ga_values.append(float(bef_val[len(bef_val)-1])*100)
-            after_ga_values_15.append(float(aft_val[len(bef_val)-1])*100)
-        if int(S_arr_len[len(S_arr_len)-1]) == S_length + 10 and len(after_ga_values_25) < 10:
-            after_ga_values_25.append(float(aft_val[len(bef_val)-1])*100)
-        if int(S_arr_len[len(S_arr_len)-1]) == S_length + 20 and len(after_ga_values_35) < 10:
-            after_ga_values_35.append(float(aft_val[len(bef_val)-1])*100)
-        if int(S_arr_len[len(S_arr_len)-1]) == S_length + 30 and len(after_ga_values_45) < 10:
-            after_ga_values_45.append(float(aft_val[len(bef_val)-1])*100)
+            after_ga_values_15.append(val_to_add)
+        if val_of_S_arr_len == S_length + 10 and len(after_ga_values_25) < 10:
+            after_ga_values_25.append(val_to_add)
+        if val_of_S_arr_len == S_length + 20 and len(after_ga_values_35) < 10:
+            after_ga_values_35.append(val_to_add)
+        if val_of_S_arr_len == S_length + 30 and len(after_ga_values_45) < 10:
+            after_ga_values_45.append(val_to_add)
         if len(after_ga_values_15) == 10 and len(after_ga_values_25) == 10 \
                 and len(after_ga_values_35) == 10 and len(after_ga_values_45) == 10:
             break
